04ed5aa24b9935b8482819a4378878a3f9c95d79
Simen Holmestad <simehol@stud.ntnu.no>
2021-10-15 12:50:46 +0200

    Change to better segmentation model
    This one has better baselines.
    
    See https://github.com/facebookresearch/detectron2/blob/main/MODEL_ZOO.md

--- video_operations/base_coco_mask_rcnn_operation.py
+++ video_operations/base_coco_mask_rcnn_operation.py
@@ -15,7 +15,7 @@ class BaseCocoMaskRcnnOperation(BaseVideoOperation, ABC):
         self.cfg = get_cfg()
         if not torch.cuda.is_available():
             self.cfg.MODEL.DEVICE = "cpu"
-        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for the model
-        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
         self.detectron2_model = DefaultPredictor(self.cfg)
